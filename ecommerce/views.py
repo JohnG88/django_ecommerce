3e81707f50b8ca9388a2f49481e91a3a96dd8cee
John Gutierrez <djterez88@gmail.com>
2022-08-24 09:16:02 -0700

    setup django project to render templates to localhost

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    hello = "hello"
+
+    context = {hello: 'hello'}
+    return render (request, 'ecommerce/index.html', context) 
