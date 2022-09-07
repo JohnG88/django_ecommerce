294406e88efea1ebbeea76bcf238634e62cb8253
John Gutierrez <djterez88@gmail.com>
2022-09-06 23:10:21 -0700

    Added UserAdmin to CustomUser in admin.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Item, ShippingAddress, CustomUser, Order, OrderItem
 # Register your models here
 admin.site.register(Item)
 admin.site.register(ShippingAddress)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -130,9 +130,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+
 
 #CORS_ALLOWED_ORIGINS = [
 #    "http://127.0.0.1:8000",
